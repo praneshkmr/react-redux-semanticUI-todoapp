f115cff9f977b9b5fbfa5e66e33f6ef00d877357
Pranesh Kumar <praneshkmr@gmail.com>
2017-06-27 10:36:57 +0530

    Basic Redux Setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import reducers from "./reducers";
+
+const store = createStore(reducers);
 
 import Main from "./Main";
 
-render(<Main />, document.getElementById("app"));
+render(
+    <Provider store={store}>
+        <Main />
+    </Provider>
+    , document.getElementById("app"));
