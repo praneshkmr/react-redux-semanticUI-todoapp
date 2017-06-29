82698c92bed66ef80294a50b151c75e050dedb66
Pranesh Kumar <praneshkmr@gmail.com>
2017-06-29 16:32:45 +0530

    Added Container for the Layout

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import { Container } from 'semantic-ui-react'
 
 import TodoForm from "./containers/TodoForm";
 import TodosList from "./containers/TodosList";
@@ -7,10 +8,10 @@ import TodosList from "./containers/TodosList";
 export default class Main extends Component {
     render() {
         return (
-            <div>
+            <Container>
                 <TodoForm />
                 <TodosList />
-            </div>
+            </Container>
         )
     }
 }
